301fb3d44d1d2b1dbc4ecb6ad3fd7d6d1994263a
Geoff Whatley <me@geoffwhatley.com>
2026-07-06 00:32:06 +1000

    feat(#164): delete service-api GraphQL gateway (#249)

--- projects/service-api/src/schema/types/avatar-class.ts
+++
@@ -1,6 +0,0 @@
-import { Class } from '@vers/data';
-import { builder } from '../builder';
-
-export const AvatarClass = builder.enumType('AvatarClass', {
-  values: Object.fromEntries(Object.entries(Class).map(([key, value]) => [key, { value }])),
-});
--- scripts/stack/types.ts
+++ scripts/stack/types.ts
@@ -9,7 +9,6 @@ export enum Command {
 
 export enum ServiceID {
   Postgres = 'postgres',
-  ServiceAPI = 'service-api',
   ServiceEmail = 'service-email',
   ServiceSession = 'service-session',
   ServiceUser = 'service-user',
