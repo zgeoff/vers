9177649f474bcbfe5ec1db017598c42f6e7ad4fc
Geoff Whatley <me@geoffwhatley.com>
2026-07-07 23:54:28 +1000

    refactor(#259): use direct member access in mock user lookup

--- projects/app-web/mocks/routers/user/get-user.ts
+++ projects/app-web/mocks/routers/user/get-user.ts
@@ -2,15 +2,12 @@ import { userCollection } from '../../db/user-collection';
 import { os } from './os';
 
 export const getUser = os.getUser.handler((opts) => {
-  const { id } = opts.input;
-  const { email } = opts.input;
-
-  if (id !== undefined) {
-    return userCollection.findFirst((q) => q.where({ id })) ?? null;
+  if (opts.input.id !== undefined) {
+    return userCollection.findFirst((q) => q.where({ id: opts.input.id })) ?? null;
   }
 
-  if (email !== undefined) {
-    return userCollection.findFirst((q) => q.where({ email })) ?? null;
+  if (opts.input.email !== undefined) {
+    return userCollection.findFirst((q) => q.where({ email: opts.input.email })) ?? null;
   }
 
   return null;
