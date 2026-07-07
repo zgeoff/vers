12490e3db58ae100ba415819150e42df77d917d5
Geoff Whatley <me@geoffwhatley.com>
2026-07-07 23:57:39 +1000

    fix(#259): hoist optional inputs to consts for closure narrowing

--- projects/app-web/mocks/routers/user/get-user.ts
+++ projects/app-web/mocks/routers/user/get-user.ts
@@ -2,12 +2,15 @@ import { userCollection } from '../../db/user-collection';
 import { os } from './os';
 
 export const getUser = os.getUser.handler((opts) => {
-  if (opts.input.id !== undefined) {
-    return userCollection.findFirst((q) => q.where({ id: opts.input.id })) ?? null;
+  const { id } = opts.input;
+  const { email } = opts.input;
+
+  if (id !== undefined) {
+    return userCollection.findFirst((q) => q.where({ id })) ?? null;
   }
 
-  if (opts.input.email !== undefined) {
-    return userCollection.findFirst((q) => q.where({ email: opts.input.email })) ?? null;
+  if (email !== undefined) {
+    return userCollection.findFirst((q) => q.where({ email })) ?? null;
   }
 
   return null;
